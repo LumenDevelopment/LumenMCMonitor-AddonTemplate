b96221093fd66069930c0cf3460848e87fd49f3b
snvksvnk <98760003+snvksnvk@users.noreply.github.com>
2026-02-11 13:40:43 +0100

    Update to b1.3

--- src/main/java/cloud/lumenvm/TemplateAddon.java
+++ src/main/java/cloud/lumenvm/TemplateAddon.java
@@ -1,6 +1,7 @@
 package cloud.lumenvm;
 
 import cloud.lumenvm.api.*;
+import cloud.lumenvm.monitor.Embed;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
